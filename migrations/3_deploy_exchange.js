017ba921843dd1914aac61255a0b9ca89b4b160a
borispovod <boris@wings.ai>
2020-08-13 15:55:45 +0300

    deploy exchange with deadline and creator address

--- migrations/3_deploy_exchange.js
+++ migrations/3_deploy_exchange.js
@@ -14,6 +14,7 @@ module.exports = async function deploy(deployer) {
     const WINGS_TOKEN_ADDRESS       = process.env.WINGS_TOKEN_ADDRESS;
     const UNISWAP_V2_ROUTER_ADDRESS = process.env.UNISWAP_V2_ROUTER_ADDRESS;
     const XFI_TOKEN_ADDRESS = process.env.XFI_TOKEN_ADDRESS;
+    const DEADLINE = process.env.DEADLINE;
 
     if (!CREATOR_ADDRESS) {
         throw 'CREATOR_ADDRESS is missing';
@@ -31,8 +32,14 @@ module.exports = async function deploy(deployer) {
         throw 'XFI_TOKEN_ADDRESS is missing';
     }
 
+    if (!DEADLINE) {
+        throw 'DEADLINE is missing';
+    }
+
     // Deploy the Exchange.
-    await deployer.deploy(Exchange, WINGS_TOKEN_ADDRESS, XFI_TOKEN_ADDRESS, UNISWAP_V2_ROUTER_ADDRESS);
+    await deployer.deploy(Exchange, WINGS_TOKEN_ADDRESS, XFI_TOKEN_ADDRESS, UNISWAP_V2_ROUTER_ADDRESS, DEADLINE, {
+        from: CREATOR_ADDRESS
+    });
 
     // Grant the Exchange role of minter.
     const xfiToken   = await XfiToken.at(XFI_TOKEN_ADDRESS);
